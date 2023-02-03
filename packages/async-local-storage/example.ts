9a559375c6a0e32dac607e1a7e84d75b07d7e5ab
Edouard <ed@getlarge.eu>
2023-02-03 07:44:59 +0100

    test: add tests for `AsyncLocalStorageService`

--- packages/async-local-storage/example.ts
+++ packages/async-local-storage/example.ts
@@ -1,3 +1,4 @@
+/* run with npx ts-node packages/async-local-storage/example.ts */
 import assert from 'assert';
 import { AsyncLocalStorage } from 'async_hooks';
 
@@ -17,14 +18,17 @@ import { AsyncLocalStorageService } from './src';
 
 const service = new AsyncLocalStorageService(new AsyncLocalStorage());
 
-assert.throws(() => service.set('id', 1), new TypeError("Cannot read properties of undefined (reading 'set')"));
+assert.throws(() => service.store.set('id', 1), new TypeError("Cannot read properties of undefined (reading 'set')"));
+assert.throws(() => service.set('id', 1), new Error("Store is not initialized. Call 'enterWith' or 'run' first."));
 
 service.enterWith(new Map());
 
-service.set('id', 1);
-const id = service.get('id');
+// access store map directly
+service.store.set('id', 1);
+const id = service.store.get('id');
 assert(typeof id === 'number');
 
+// access store map via service which extends Map
 service.set('username', 'john');
 assert(typeof service.get('username') === 'string');
 
--- packages/async-local-storage/test/async-local-storage.spec.ts
+++ packages/async-local-storage/test/async-local-storage.spec.ts
@@ -1,7 +1,8 @@
 /* eslint-disable max-lines-per-function */
-import { Provider } from '@nestjs/common';
+import { INestApplication, Provider } from '@nestjs/common';
 import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { Test, TestingModule } from '@nestjs/testing';
+import { AsyncLocalStorage } from 'async_hooks';
 import request from 'supertest';
 
 import {
@@ -9,6 +10,7 @@ import {
   AsyncLocalStorageInterceptor,
   AsyncLocalStorageModule,
   AsyncLocalStorageModuleOptions,
+  AsyncLocalStorageService,
 } from '../src';
 import { ExampleController } from './app.controller.mock';
 import { ExampleService } from './app.service.mock';
@@ -30,12 +32,17 @@ const moduleFactory = async (options: AsyncLocalStorageModuleOptions, providers:
   return module.createNestApplication();
 };
 
-describe('AsyncLocalStorage', () => {
+describe('AsyncLocalStorageModule', () => {
+  let app: INestApplication;
   const route = '/Example';
   const expectedResponse = { type: 'http' };
 
+  afterEach(() => {
+    return app?.close();
+  });
+
   it('should provide request context when using explicit guard', async () => {
-    const app = await moduleFactory(
+    app = await moduleFactory(
       {
         isGlobal: true,
         requestContextFactory: (ctx) => ({ type: ctx.getType() }),
@@ -45,12 +52,10 @@ describe('AsyncLocalStorage', () => {
     //
     const { body } = await request(app.getHttpServer()).get(route);
     expect(body).toEqual(expectedResponse);
-    //
-    await app.close();
   });
 
   it('should provide request context when using implicit guard', async () => {
-    const app = await moduleFactory({
+    app = await moduleFactory({
       isGlobal: true,
       requestContextFactory: (ctx) => ({ type: ctx.getType() }),
       useGuard: true,
@@ -58,12 +63,10 @@ describe('AsyncLocalStorage', () => {
     //
     const { body } = await request(app.getHttpServer()).get(route);
     expect(body).toEqual(expectedResponse);
-    //
-    await app.close();
   });
 
   it('should provide request context when using explicit interceptor', async () => {
-    const app = await moduleFactory(
+    app = await moduleFactory(
       {
         isGlobal: true,
         requestContextFactory: (ctx) => ({ type: ctx.getType() }),
@@ -73,12 +76,10 @@ describe('AsyncLocalStorage', () => {
     //
     const { body } = await request(app.getHttpServer()).get(route);
     expect(body).toEqual(expectedResponse);
-    //
-    await app.close();
   });
 
   it('should provide request context when using implicit interceptor', async () => {
-    const app = await moduleFactory({
+    app = await moduleFactory({
       isGlobal: true,
       requestContextFactory: (ctx) => ({ type: ctx.getType() }),
       useInterceptor: true,
@@ -86,8 +87,6 @@ describe('AsyncLocalStorage', () => {
     //
     const { body } = await request(app.getHttpServer()).get(route);
     expect(body).toEqual(expectedResponse);
-    //
-    await app.close();
   });
 
   it('should throw when not providing `requestContextFactory`', async () => {
@@ -102,5 +101,65 @@ describe('AsyncLocalStorage', () => {
         useInterceptor: true,
       }),
     ).rejects.toThrowError("Can't use both guard and interceptor.");
+  });
+});
+
+describe('AsyncLocalStorageService', () => {
+  let service: AsyncLocalStorageService;
+  beforeEach(() => {
+    service = new AsyncLocalStorageService(new AsyncLocalStorage());
+  });
+
+  afterEach(() => {
+    service?.exit();
+  });
+
+  it('should throw when accessing store before initialization', async () => {
+    expect(() => service.set('ctx', { type: '' })).toThrowError(
+      "Store is not initialized. Call 'enterWith' or 'run' first.",
+    );
+  });
+
+  it('should allow to set/get/delete store properties once initialized', async () => {
+    service.enterWith(new Map());
+    //
+    service.set('ctx', { type: '' });
+    const ctx = service.get('ctx');
+    expect(typeof ctx.type).toBe('string');
+    expect(typeof service.requestContext.type).toBe('string');
+    service.delete('ctx');
+    expect(typeof service.requestContext).toBe('undefined');
+  });
+
+  it('should allow to access all map properties and methods once initialized', async () => {
+    service.enterWith(new Map());
+    //
+    expect(service.has('ctx')).toBeFalsy();
+    expect(service.size).toBe(0);
+    service.set('ctx', { type: '' });
+    expect(service.has('ctx')).toBeTruthy();
+    expect(service.size).toBe(1);
+    expect(Array.from(service.keys())).toEqual(['ctx']);
+    expect(Array.from(service.values())).toEqual([{ type: '' }]);
+    service.clear();
+    expect(service.size).toBe(0);
+  });
+
+  it('should allow to run a function with store', async () => {
+    service.run(new Map(), () => {
+      service.set('ctx', { type: '' });
+      expect(service.get('ctx')).toEqual({ type: '' });
+    });
+    expect(service.store).toBeUndefined();
+  });
+
+  it('should contain the same store in static and instance members', async () => {
+    const requestContext = { type: '' };
+    service.enterWith(new Map());
+    service.set('ctx', requestContext);
+    expect(service.requestContext).toEqual(requestContext);
+    expect(service.get('ctx')).toEqual(requestContext);
+    expect(AsyncLocalStorageService.requestContext).toEqual(requestContext);
+    expect(AsyncLocalStorageService.store.get('ctx')).toEqual(requestContext);
   });
 });

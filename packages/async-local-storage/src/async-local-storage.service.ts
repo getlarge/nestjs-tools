dbf9863fecde46dfa374e11a875fb75d0647d4af
Edouard <ed@getlarge.eu>
2023-02-03 05:37:53 +0100

    feat(async-local-storage): extends Map in `AsyncLocalStorageService`

--- packages/async-local-storage/src/async-local-storage.service.ts
+++ packages/async-local-storage/src/async-local-storage.service.ts
@@ -2,14 +2,19 @@ import { Inject, Injectable } from '@nestjs/common';
 import { AsyncLocalStorage } from 'async_hooks';
 
 import { ASYNC_LOCAL_STORAGE, noOp, REQUEST_CONTEXT_KEY } from './async-local-storage.constants';
-import { ContextStore, RequestContext } from './async-local-storage.interfaces';
+import { ContextStoreProperties } from './async-local-storage.interfaces';
+
+type K = keyof ContextStoreProperties;
+type T = ContextStoreProperties;
+export type StoreMap = Map<K, T[K]>;
 
 @Injectable()
-export class AsyncLocalStorageService {
-  readonly instance: AsyncLocalStorage<ContextStore>;
-  private static _instance: AsyncLocalStorage<ContextStore>;
+export class AsyncLocalStorageService extends Map<K, T[K]> {
+  readonly instance: AsyncLocalStorage<StoreMap>;
+  private static _instance: AsyncLocalStorage<StoreMap>;
 
-  constructor(@Inject(ASYNC_LOCAL_STORAGE) instance: AsyncLocalStorage<ContextStore>) {
+  constructor(@Inject(ASYNC_LOCAL_STORAGE) instance: AsyncLocalStorage<StoreMap>) {
+    super();
     // ensure that the instance is a singleton
     if (!AsyncLocalStorageService.instance) {
       AsyncLocalStorageService.instance = instance;
@@ -18,27 +23,27 @@ export class AsyncLocalStorageService {
     this.instance = AsyncLocalStorageService.instance;
   }
 
-  static get instance(): AsyncLocalStorage<ContextStore> {
+  static get instance(): AsyncLocalStorage<StoreMap> {
     return this._instance || globalThis.__asyncstorage;
   }
 
-  static set instance(value: AsyncLocalStorage<ContextStore>) {
+  static set instance(value: AsyncLocalStorage<StoreMap> | undefined) {
     this._instance = value;
   }
 
-  static get store(): ContextStore {
+  private static get store() {
     return this.instance?.getStore();
   }
 
-  static get requestContext(): RequestContext {
-    return this.store?.get(REQUEST_CONTEXT_KEY);
+  static get requestContext(): T[typeof REQUEST_CONTEXT_KEY] {
+    return this.store.get(REQUEST_CONTEXT_KEY) as T[typeof REQUEST_CONTEXT_KEY];
   }
 
-  static set requestContext(value: RequestContext) {
-    this.store?.set(REQUEST_CONTEXT_KEY, value);
+  static set requestContext(value: T[typeof REQUEST_CONTEXT_KEY]) {
+    this.store.set(REQUEST_CONTEXT_KEY, value);
   }
 
-  static enterWith(value: ContextStore = new Map()): void {
+  static enterWith(value: StoreMap = new Map()): void {
     this.instance?.enterWith(value);
   }
 
@@ -50,27 +55,78 @@ export class AsyncLocalStorageService {
     this.instance?.exit(cb);
   }
 
-  get store(): ContextStore {
-    return AsyncLocalStorageService.store;
+  // AsyncLocalStorage methods and properties
+  get store(): StoreMap {
+    return this.instance.getStore();
   }
 
-  get requestContext(): RequestContext {
-    return AsyncLocalStorageService.requestContext;
-  }
-
-  set requestContext(value: RequestContext) {
-    AsyncLocalStorageService.requestContext = value;
-  }
-
-  enterWith(value: ContextStore = new Map()): void {
-    AsyncLocalStorageService.enterWith(value);
+  enterWith(value: StoreMap = new Map()): void {
+    this.instance.enterWith(value);
   }
 
   enter(): void {
-    AsyncLocalStorageService.enter();
+    this.enterWith();
   }
 
   exit(cb: () => void = noOp): void {
-    AsyncLocalStorageService.exit(cb);
+    this.instance.exit(cb);
+  }
+
+  // Map methods and properties
+  get<k extends K>(key: k): T[k] | undefined {
+    return this.store.get(key) as T[k];
+  }
+
+  set<k extends K>(key: k, value: T[k]): this {
+    this.store.set(key, value);
+    return this;
+  }
+
+  delete(key: K): boolean {
+    return this.store.delete(key);
+  }
+
+  has(key: K): boolean {
+    return this.store.has(key);
+  }
+
+  clear(): void {
+    return this.store.clear();
+  }
+
+  get size(): number {
+    return this.store.size;
+  }
+
+  get [Symbol.toStringTag]() {
+    return this.store[Symbol.toStringTag];
+  }
+
+  keys(): IterableIterator<K> {
+    return this.store.keys();
+  }
+
+  values(): IterableIterator<T[keyof T]> {
+    return this.store.values();
+  }
+
+  entries(): IterableIterator<[K, T[K]]> {
+    return this.store.entries();
+  }
+
+  forEach(callbackfn: (value: T[K], key: K, map: Map<K, T[K]>) => void, thisArg?: any): void {
+    return this.store.forEach(callbackfn, thisArg);
+  }
+
+  [Symbol.iterator](): IterableIterator<[K, T[K]]> {
+    return this.store[Symbol.iterator]();
+  }
+
+  get requestContext(): T[typeof REQUEST_CONTEXT_KEY] {
+    return this.get(REQUEST_CONTEXT_KEY);
+  }
+
+  set requestContext(value: T[typeof REQUEST_CONTEXT_KEY]) {
+    this.set(REQUEST_CONTEXT_KEY, value);
   }
 }

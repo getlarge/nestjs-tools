e66e7802ebc96fc66ae618799023bd2231477136
Eamon <eamon0989@gmail.com>
2023-04-03 12:57:29 +0200

    ci: get S3_ENDPOINT from secrets.S3_ENDPOINT

--- packages/file-storage/test/file-storage.e2e-spec.ts
+++ packages/file-storage/test/file-storage.e2e-spec.ts
@@ -43,6 +43,7 @@ const testMap: {
           maxPayloadSize: 1,
           bucket: process.env.S3_BUCKET,
           region: process.env.S3_REGION,
+          endpoint: process.env.S3_ENDPOINT,
           credentials: {
             accessKeyId: process.env.S3_ACCESS_KEY_ID,
             secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,

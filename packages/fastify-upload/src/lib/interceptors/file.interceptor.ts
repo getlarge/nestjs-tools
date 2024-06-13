dbfb6e9fccc3a3701ff5f6c3c952bcc9c539efed
getlarge <ed@getlarge.eu>
2024-06-13 10:29:07 +0200

    feat(nestjs-tools-fastify-upload): add decoratos and interceptors

---
+++ packages/fastify-upload/src/lib/interceptors/file.interceptor.ts
@@ -0,0 +1,32 @@
+import { CallHandler, ExecutionContext, mixin, NestInterceptor, Type } from '@nestjs/common';
+import { Observable, tap } from 'rxjs';
+
+import { handleMultipartSingleFile } from '../multipart/handlers/single-file';
+import { type TransformedUploadOptions, transformUploadOptions, type UploadOptions } from '../multipart/options';
+import { getMultipartRequest } from '../multipart/request';
+import type { Storage } from '../storage';
+
+export function FileInterceptor<S extends Storage>(
+  fieldname: string,
+  options?: UploadOptions<S>,
+): Type<NestInterceptor> {
+  class MixinInterceptor implements NestInterceptor {
+    private readonly options: TransformedUploadOptions<S>;
+
+    constructor() {
+      this.options = transformUploadOptions(options);
+    }
+
+    async intercept(context: ExecutionContext, next: CallHandler): Promise<Observable<void>> {
+      const ctx = context.switchToHttp();
+      const req = getMultipartRequest(ctx);
+
+      const { file, body, remove } = await handleMultipartSingleFile(req, fieldname, this.options);
+      req.body = body;
+      req.storageFile = file;
+      return next.handle().pipe(tap(remove));
+    }
+  }
+
+  return mixin(MixinInterceptor);
+}

62629d96a57dc011eecc3cf27a3611f95ba02a1c
getlarge <ed@getlarge.eu>
2024-08-01 18:14:19 +0200

    test(nestjs-tools-file-storage): make tests independent

--- packages/file-storage/test/file-storage.spec.ts
+++ packages/file-storage/test/file-storage.spec.ts
@@ -2,6 +2,7 @@
 /* eslint-disable max-lines-per-function */
 import { Test, TestingModule } from '@nestjs/testing';
 import * as dotenv from 'dotenv';
+import { randomBytes, randomUUID } from 'node:crypto';
 import { mkdir, rm } from 'node:fs/promises';
 import { resolve } from 'node:path';
 import { once, Readable } from 'node:stream';
@@ -29,13 +30,6 @@ declare global {
 }
 
 const storagePath = resolve('store');
-// const path = resolve(storagePath);
-const testFileName = 'test.txt';
-const testFileContent = 'this is a test';
-const dirPath = '';
-const nestedDir = 'nested';
-const nestedFileName = 'nested.txt';
-const nestedFilePath = `${storagePath}/${nestedDir}`;
 
 const testMap: {
   description: string;
@@ -100,7 +94,7 @@ testMap.forEach((testSuite) => {
       fileStorage = module.get(FILE_STORAGE_STRATEGY_TOKEN);
       if (storageType === StorageType.FS) await mkdir(storagePath, { recursive: true });
       // ensure S3 and GC buckets are empty
-      if ([StorageType.S3, StorageType.GC].includes(storageType)) await fileStorage.deleteDir({ dirPath });
+      if ([StorageType.S3, StorageType.GC].includes(storageType)) await fileStorage.deleteDir({ dirPath: '' });
     });
 
     afterAll(async () => {
@@ -108,85 +102,134 @@ testMap.forEach((testSuite) => {
         await rm(storagePath, { recursive: true, force: true }).catch(() => {
           // ignore error
         });
-      if ([StorageType.S3, StorageType.GC].includes(storageType)) await fileStorage.deleteDir({ dirPath });
-    });
-
-    it('readDir returns an empty array when no files exist', async () => {
-      const res = await fileStorage.readDir({ dirPath });
-      expect(res.length).toBe(0);
-    });
-
-    it('uploadFile uploads a file', async () => {
-      await fileStorage.uploadFile({ filePath: testFileName, content: 'this is a test' });
-      const result = await fileStorage.readDir({ dirPath });
-      expect(result.length).toBe(1);
-      expect(result[0]).toBe(testFileName);
+      if ([StorageType.S3, StorageType.GC].includes(storageType)) await fileStorage.deleteDir({ dirPath: '' });
     });
 
     it('calling fileExists on a filepath that exists returns true', async () => {
-      const fileExists = await fileStorage.fileExists({ filePath: testFileName });
+      const filePath = randomUUID();
+      await fileStorage.uploadFile({ filePath, content: 'this is a test' });
+      //
+      const fileExists = await fileStorage.fileExists({ filePath });
       expect(fileExists).toBe(true);
+      await fileStorage.deleteFile({ filePath });
     });
 
     it('calling fileExists on a filepath that doesnt exist return false', async () => {
       const fileExists = await fileStorage.fileExists({ filePath: 'fileDoesntExist' });
+      //
       expect(fileExists).toBe(false);
     });
 
+    it('readDir returns an empty array when no files exist', async () => {
+      const res = await fileStorage.readDir({ dirPath: '' });
+      expect(res.length).toBe(0);
+    });
+
+    it('uploadFile uploads a file', async () => {
+      const filePath = randomUUID();
+      await fileStorage.uploadFile({ filePath, content: 'this is a test' });
+      //
+      const fileExists = await fileStorage.fileExists({ filePath });
+      expect(fileExists).toBe(true);
+      await fileStorage.deleteFile({ filePath });
+    });
+
     it('deleteFile deletes a file', async () => {
-      await fileStorage.deleteFile({ filePath: testFileName });
-      const result = await fileStorage.readDir({ dirPath });
-      expect(result.length).toBe(0);
+      const filePath = randomUUID();
+      await fileStorage.uploadFile({ filePath, content: 'this is a test' });
+      //
+      await fileStorage.deleteFile({ filePath });
+      //
+      const oldFileExists = await fileStorage.fileExists({ filePath });
+      expect(oldFileExists).toBe(false);
     });
 
     it('uploadStream uploads a file', async () => {
-      const upload = await fileStorage.uploadStream({ filePath: testFileName });
-      const entry = Readable.from(Buffer.from(testFileContent));
+      const filePath = randomUUID();
+      const content = randomBytes(1024);
+      //
+      const upload = await fileStorage.uploadStream({ filePath });
+      const entry = Readable.from(content);
       const ac = new AbortController();
       const t = setTimeout(() => ac.abort(), 1000);
       const listener = once(upload, 'done', { signal: ac.signal }).finally(() => clearTimeout(t));
       await pipeline(entry, upload);
       await listener;
-
-      const result = await fileStorage.readDir({ dirPath });
-      expect(result.length).toBe(1);
+      //
+      const fileExists = await fileStorage.fileExists({ filePath });
+      expect(fileExists).toBe(true);
+      await fileStorage.deleteFile({ filePath });
     });
 
     it('downloadFile downloads a file', async () => {
-      const file = await fileStorage.downloadFile({ filePath: testFileName });
-      expect(file.toString()).toBe(testFileContent);
+      const filePath = randomUUID();
+      const content = randomBytes(1024);
+      await fileStorage.uploadFile({ filePath, content });
+      //
+      const file = await fileStorage.downloadFile({ filePath });
+      //
+      expect(file.toString()).toEqual(content.toString());
+      await fileStorage.deleteFile({ filePath });
     });
 
     it('downloadStream downloads a file', async () => {
-      const download = await fileStorage.downloadStream({ filePath: testFileName });
+      const filePath = randomUUID();
+      const content = randomBytes(10).toString();
+      await fileStorage.uploadFile({ filePath, content });
+      //
+      const download = await fileStorage.downloadStream({ filePath });
+      //
       expect(download).toBeInstanceOf(Readable);
       // this makes the assumption that the stream is readable and all the data is available in one read
+      // eslint-disable-next-line no-unreachable-loop
       for await (const chunk of download) {
-        expect(chunk.toString()).toBe(testFileContent);
+        expect(chunk.toString()).toBe(content);
+        break;
       }
+      await fileStorage.deleteFile({ filePath });
     });
 
     it('uploads a file to a nested folder', async () => {
+      const nestedDir = 'nested';
+      const nestedFileName = 'nested.txt';
+      const nestedFilePath = `${storagePath}/${nestedDir}`;
       if (storageType === StorageType.FS) await mkdir(nestedFilePath, { recursive: true });
-
+      //
       await fileStorage.uploadFile({ filePath: `${nestedDir}/${nestedFileName}`, content: 'this is a nested file' });
+      //
       const result = await fileStorage.readDir({ dirPath: nestedDir });
-      expect(result.length).toBe(1);
-      expect(result[0]).toBe(nestedFileName);
+      expect(result.find((fileName) => fileName === nestedFileName)).not.toBeUndefined();
+      await fileStorage.deleteDir({ dirPath: nestedDir });
     });
 
     it('readDir returns an array of files and folders in a dir', async () => {
+      const dirPath = '';
+      const filePath = randomUUID();
+      const nestedFilePath = `nest/${randomUUID()}`;
+      const content = randomBytes(1024);
+      if (storageType === StorageType.FS) await mkdir(`${storagePath}/nest`, { recursive: true });
+
+      await fileStorage.uploadFile({ filePath, content });
+      await fileStorage.uploadFile({ filePath: nestedFilePath, content });
+      //
+      const result = await fileStorage.readDir({ dirPath });
+      //
+      expect(result.length).toBe(2);
       if (storageType === StorageType.GC) {
         console.warn('GC storage readDir is not completely implemented yet. Skipping test.');
-        return;
+        // expect(result).toEqual([nestedFilePath, filePath]);
+      } else {
+        const parts = nestedFilePath.split('/');
+        const expected = [parts.shift(), filePath];
+        expect(result.every((item) => expected.includes(item))).toBe(true);
       }
-      const result = await fileStorage.readDir({ dirPath });
-      expect(result.length).toBe(2);
-      expect(result).toEqual([nestedDir, testFileName]);
     });
 
     it('deleteDir deletes a dir', async () => {
+      const dirPath = '';
+      //
       await fileStorage.deleteDir({ dirPath });
+      //
       expect(await fileStorage.readDir({ dirPath })).toEqual([]);
     });
   });

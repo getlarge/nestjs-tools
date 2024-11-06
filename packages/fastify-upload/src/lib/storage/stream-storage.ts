07c26844e4c132a4148f4bf9e89ce97f7134e531
getlarge <ed@getlarge.eu>
2024-11-06 10:21:05 +0100

    fix(nestjs-tools-fastify-upload): implement temporary file handling and improve stream processing

--- packages/fastify-upload/src/lib/storage/stream-storage.ts
+++ packages/fastify-upload/src/lib/storage/stream-storage.ts
@@ -1,5 +1,9 @@
-import { PassThrough, Readable } from 'node:stream';
-import { finished } from 'node:stream/promises';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { rm } from 'node:fs/promises';
+import { tmpdir } from 'node:os';
+import { normalize, resolve, sep } from 'node:path';
+import { Readable } from 'node:stream';
+import { finished, pipeline } from 'node:stream/promises';
 
 import { MultipartFile, Storage, StorageFile } from './storage';
 
@@ -7,11 +11,25 @@ export interface StreamStorageFile extends StorageFile {
   stream: Readable;
 }
 
+const temporaryFileOutput = (filename: string) => {
+  const safeFileName = normalize(filename).replace(/^(\.\.(\/|\\|$))+/, '');
+  const fullPath = resolve(tmpdir(), safeFileName);
+  // Ensure the resolved path starts with the intended storagePath to prevent path traversal
+  if (!fullPath.startsWith(resolve(tmpdir() + sep))) {
+    throw new Error('Invalid file path');
+  }
+  return fullPath;
+};
 export class StreamStorage extends Storage<StreamStorageFile> {
-  handleFile(file: MultipartFile): Promise<StreamStorageFile> {
+  async handleFile(file: MultipartFile): Promise<StreamStorageFile> {
     const { encoding, mimetype, fieldname } = file;
-    const stream = file.file.pipe(new PassThrough());
-
+    /**
+     * force the stream to be consumed as required by Fastify and Busboy
+     * @see https://github.com/fastify/fastify-multipart?tab=readme-ov-file#usage
+     *  */
+    const output = temporaryFileOutput(file.filename);
+    await pipeline(file.file, createWriteStream(output));
+    const stream = createReadStream(output);
     return Promise.resolve({
       size: file.file.readableLength,
       stream,
@@ -26,10 +44,11 @@ export class StreamStorage extends Storage<StreamStorageFile> {
     if (!force) {
       await finished(file.stream);
       file.stream.destroy();
-      return;
-    }
-    if (!file.stream.destroyed) {
+    } else if (!file.stream.destroyed) {
       file.stream.destroy();
     }
+    await rm(temporaryFileOutput(file.originalFilename)).catch(() => {
+      // ignore
+    });
   }
 }
